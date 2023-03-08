826a9b80d5a262e2d757f0ae1022521519b36080
Blain Muema <blainmasaku2@gmail.com>
2023-03-08 09:07:18 +0000

    Correct indentation

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for a in "abcdefghijklmnopqrstuvwxyz":
-        if(a not in "qe"):
-                    print("{}".format(a), end='')
+    if(a not in "qe"):
+        print("{}".format(a), end='')
