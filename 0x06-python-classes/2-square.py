69036089ae23448dd5ce981941b897335d43931c
octocatblain <blainmasaku2@gmail.com>
2023-08-28 15:21:07 +0300

    size validation

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+class Square:
+    """Class Square object initialized with size."""
+
+    pass
+
+    def __init__(self, size=0):
+        """init method of class Square."""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
