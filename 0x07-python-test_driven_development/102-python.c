975cc9e7e9574cc02a7c37300c0424416b71d657
octocatblain <blainmasaku2@gmail.com>
2023-09-04 12:05:49 +0300

    CPython #3: Python Strings

---
+++ 0x07-python-test_driven_development/102-python.c
@@ -0,0 +1,11 @@
+#include <Python.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_python_string - prints info about python strings
+ * @p: pointer to PyObject
+ */
+void print_python_string(PyObject *p)
+{
+}
