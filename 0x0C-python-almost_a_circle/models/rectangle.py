745af02b895ea2b47f7a8eadbadf1ba27019c9e5
octocatblain <blainmasaku2@gmail.com>
2023-09-18 14:48:44 +0300

    Update the class Rectangle by adding validation of all setter methods and instantiation (id excluded)

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -89,9 +89,9 @@ class Rectangle(Base):
             return
 
         [print("") for y in range(self.y)]
-        for h in range(self.height):
+        for _ in range(self.height):
             [print(" ", end="") for x in range(self.x)]
-            [print("#", end="") for w in range(self.width)]
+            [print("#", end="") for _ in range(self.width)]
             print("")
 
     def update(self, *args, **kwargs):
@@ -105,9 +105,8 @@ class Rectangle(Base):
                 - 5th argument represents y attribute
             **kwargs (dict): New key/value pairs of attributes.
         """
-        if args and len(args) != 0:
-            a = 0
-            for arg in args:
+        if args:
+            for a, arg in enumerate(args):
                 if a == 0:
                     if arg is None:
                         self.__init__(self.width, self.height, self.x, self.y)
@@ -121,19 +120,17 @@ class Rectangle(Base):
                     self.x = arg
                 elif a == 4:
                     self.y = arg
-                a += 1
-
         elif kwargs and len(kwargs) != 0:
             for k, v in kwargs.items():
-                if k == "id":
+                if k == "height":
+                    self.height = v
+                elif k == "id":
                     if v is None:
                         self.__init__(self.width, self.height, self.x, self.y)
                     else:
                         self.id = v
                 elif k == "width":
                     self.width = v
-                elif k == "height":
-                    self.height = v
                 elif k == "x":
                     self.x = v
                 elif k == "y":
@@ -151,6 +148,4 @@ class Rectangle(Base):
 
     def __str__(self):
         """Return the print() and str() representation of the Rectangle."""
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
-                                                       self.x, self.y,
-                                                       self.width, self.height)
+        return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
