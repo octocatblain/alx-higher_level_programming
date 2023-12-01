3620cb0cdb59d762057c94af48c6afa8f60a93da
Blain Muema <blainmuema@gmail.com>
2023-12-01 04:02:37 -0800

    Task 2.Response header value #0

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
-#  script that takes in a URL, sends a request to the URL and displays the value of the X-Request-Id variable found in the header of the response.
+""" This script takes in a URL, sends a request to the URL and displays the value of the X-Request-Id variable found in the header of the response."""
 
-import urllib.request
-import sys
+from urllib.request import urlopen, Request
+from urllib.error import URLError
+from sys import argv
 
-def get_x_request_id(url):
+
+def get_header_value(url_str: str):
     try:
-        with urllib.request.urlopen(url) as response:
-            headers = response.headers
-            x_request_id = headers.get('X-Request-Id')
-            return x_request_id
-    except urllib.error.HTTPError as e:
-        return "Error: {}".format(e)
+        req = Request(url_str)
+        with urlopen(req) as response:
+            body = response.read()
+        print(response.info().get("X-Request-Id"))
+    except URLError as e:
+        if hasattr(e, "reason"):
+            print(e.reason)
+        elif hasattr(e, "code"):
+            print(e.code)
+
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: {} <URL>".format(sys.argv[0]))
-        sys.exit(1)
-
-    url = sys.argv[1]
-    x_request_id = get_x_request_id(url)
-    print(x_request_id)
+    get_header_value(argv[1])
