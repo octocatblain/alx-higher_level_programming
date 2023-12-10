6c1515e5e98c323455519ceb3ad0f414ecf781cf
Blain Muema <blainmasaku2@gmail.com>
2023-12-10 12:23:31 +0000

    correction for task 1. Square with size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,21 @@
 #!/usr/bin/python3
-class Square:
-    """class Square with private instance attribute size."""
+"""A class defining a square (based on 1-square.py)"""
 
-    def __init__(self, size):
+class Square:
+    """
+    Class Square with private instance attribute size.
+
+    Attributes:
+        __size (int): The size of the square.
+
+    """
+
+    def __init__(self, size=0):
+        """
+        Initialize the square with a given size.
+
+        Args:
+            size (int): The size of the square.
+
+        """
         self.__size = size
