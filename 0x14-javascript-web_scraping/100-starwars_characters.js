88bcb0825147758ac859f6816dfb82988f9a833a
Blain Muema <blainmasaku2@gmail.com>
2024-01-03 12:16:28 +0000

    task 7. who was playing in this movie?

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,16 +1,20 @@
 #!/usr/bin/node
 const request = require('request');
-const film = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/${film}`;
-request(url, (err, res, body) => {
-  if (!err) {
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (!error) {
     const characters = JSON.parse(body).characters;
-    characters.forEach((character) => {
-      request(character, function (err, res, body) {
-        if (!err) {
-          console.log(JSON.parse(body).name);
-        }
-      });
-    });
+    printCharacters(characters, 0);
   }
 });
+
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
+    }
+  });
+}
